93fa76a8371c1ac26d5567a92c520bad85e26603
HamidAlfatemi <hamid_alfa@proton.me>
2025-10-12 11:48:55 +0330

    Oct 12 2025

--- src/cytoscape-config.js
+++
@@ -1,4 +0,0 @@
-import cytoscape from '../longevity/staticfiles/cytoscape/dist/cytoscape.min.js';
-import cola from '../longevity/staticfiles/cytoscape-cola/cytoscape-cola.js';
-cytoscape.use(cola);
-export default cytoscape;
